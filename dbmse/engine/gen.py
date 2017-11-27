4dcc689d9141f49506e649c330d9a2902f19cde5
Egor Suvorov <egor.suvorov@gmail.com>
2017-11-27 19:14:10 +0300

    Update gen.py

--- dbmse/engine/gen.py
+++ dbmse/engine/gen.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 from random import *
 print(2)
 print("data id")
 print("STRING INT")
 print("UNKNOWN UNKNOWN")
 for _ in range(100):
-  print("x" + str(randint(1, 10)), randint(1, 10))
+  print("x{} {}".format(randint(1, 20), randint(1, 20)))
