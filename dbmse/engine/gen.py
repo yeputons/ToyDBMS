a4780089b372b0c6a1a8be62c9d0471769af7708
Egor Suvorov <egor.suvorov@gmail.com>
2018-01-26 14:25:52 +0300

    gen.py: add comment about usage

--- dbmse/engine/gen.py
+++ dbmse/engine/gen.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 from random import *
+# Usage: generate 'big1' and 'big2', use sqlite to
+# generate answer for 'big' queries in tests (sorted),
+# run tests, check sorted output.
 print(2)
 print("data id")
 print("STRING INT")
